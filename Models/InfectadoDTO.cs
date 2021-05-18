dd6b6ca2bdacf04bf4b388faa84eea533baf601d
carlos brandini <carlos_brandini@yahoo.com.br>
2021-05-17 23:47:16 -0300

    commit inicial.

---
+++ Models/InfectadoDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Api.Models
+{
+    public class InfectadoDTO
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Laboratório Digital Innovation One
+
+## Construindo um projeto de uma API.NET integrada ao MongoDB
+
+Projeto do laboratório da digital innovation one.
